0f817a0cd0b41ba35c1840a6738753cd6c735dc0
ttoghere <tuncsoftwarehere@gmail.com>
2022-11-30 04:15:05 +0300

    Testing Starts

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 import 'dart:io';
-
-import 'package:bloc/bloc.dart';
+import 'package:blocgdetutorial/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'dart:developer' as devtools show log;
 
-import 'firebase_options.dart';
+//Oluşan veri değişimlerini sergileyen bir test methodu
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,7 +28,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
+      //Bloc Entegre yolu temel seviye
+      home: BlocProvider(
+        create: (_) => PersonBloc(),
+        child: const HomeScreen(),
+      ),
     );
   }
 }
@@ -49,6 +56,7 @@ enum PersonUrl {
 }
 
 //Hosting yokken LiveServer extension ile simule edilebilir
+//extension sonunda kullanılan on takısı ardından gelen Sınıf için geçerlidir
 extension UrlString on PersonUrl {
   String get urlString {
     switch (this) {
@@ -74,6 +82,9 @@ class Person {
   Person.fromJson(Map<String, dynamic> json)
       : name = json["name"] as String,
         age = json["age"];
+
+  @override
+  String toString() => "Person(name: $name, age: $age)";
 }
 
 //Local host get request
@@ -95,15 +106,44 @@ class FetchResult {
   });
 
   @override
-  String toString() {
-    "FetchResult (isRetrievedFromCache = $isRetrievedFromCache, persons = $persons)";
-    return super.toString();
+  String toString() =>
+      "FetchResult (isRetrievedFromCache = $isRetrievedFromCache, persons = $persons)";
+}
+
+//Temel Bloc Tanımlaması (Event,State)
+class PersonBloc extends Bloc<LoadAction, FetchResult?> {
+  final Map<PersonUrl, Iterable<Person>> _cache = {};
+  PersonBloc() : super(null) {
+    //Bloc içinde event belirleme
+    on<LoadPersonsAction>(
+      (event, emit) async {
+        //todo
+        final url = event.url;
+        if (_cache.containsKey(url)) {
+          //Cache içinde bulunan veri
+          final cachedPersons = _cache[url]!;
+          final result = FetchResult(
+            persons: cachedPersons,
+            isRetrievedFromCache: true,
+          );
+          emit(result);
+        } else {
+          final persons = await getPersons(url.urlString);
+          _cache[url] = persons;
+          final result = FetchResult(
+            persons: persons,
+            isRetrievedFromCache: false,
+          );
+          emit(result);
+        }
+      },
+    );
   }
 }
 
-class PersonBloc extends Bloc<LoadAction, FetchResult?> {
-  final Map<PersonUrl, Iterable<Person>> _cache = {};
-  PersonBloc() : super(null);
+//Iterable list sınıfının miras aldığı temeldir
+extension Subscript<T> on Iterable<T> {
+  T? operator [](int index) => length > index ? elementAt(index) : null;
 }
 
 class HomeScreen extends StatelessWidget {
@@ -111,10 +151,57 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    late final Bloc myBloc;
     return Scaffold(
       appBar: AppBar(
         title: Text("Home Page"),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  context.read<PersonBloc>().add(
+                        const LoadPersonsAction(url: PersonUrl.persons1),
+                      );
+                },
+                child: const Text("Load Persons1.json"),
+              ),
+              TextButton(
+                onPressed: () {
+                  context.read<PersonBloc>().add(
+                        const LoadPersonsAction(url: PersonUrl.persons2),
+                      );
+                },
+                child: const Text("Load Persons2.json"),
+              ),
+            ],
+          ),
+          BlocBuilder<PersonBloc, FetchResult?>(
+            //Builder parametresinin çalışma şartını belirler
+            buildWhen: (previous, current) {
+              return previous?.persons != current?.persons;
+            },
+            builder: (context, state) {
+              state?.log();
+              final persons = state?.persons;
+              if (persons == null) {
+                return const SizedBox();
+              }
+              return Expanded(
+                child: ListView.builder(
+                  itemCount: persons.length,
+                  itemBuilder: (context, index) {
+                    final person = persons[index]!;
+                    return ListTile(
+                      title: Text(person.name),
+                    );
+                  },
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
